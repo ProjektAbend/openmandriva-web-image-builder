e7fa893e60e34ad6f72d9a2d28e14e29a070ee39
Ronny Falconeri <ronny.falconeri@prodyna.com>
2023-08-27 14:56:32 +0200

    FE: architecture input created, grid styling added

--- image-builder-frontend/src/app/app.module.ts
+++ image-builder-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConfigPageComponent } from './pages/config-page/config-page.component';
+import { ArchitectureInputComponent } from './components/architecture-input/architecture-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfigPageComponent
+    ConfigPageComponent,
+    ArchitectureInputComponent
   ],
   imports: [
     BrowserModule,
---
+++ image-builder-frontend/src/app/components/architecture-input/architecture-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-architecture-input',
+  templateUrl: './architecture-input.component.html',
+  styleUrls: ['./architecture-input.component.scss']
+})
+export class ArchitectureInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
